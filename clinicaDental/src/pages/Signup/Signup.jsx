29371c2adbae6144a0a2b0f30795145996c665d7
DitGrau <juditgraup@gmail.com>
2023-06-25 23:02:47 +0200

    "feat: signup usuarios"

---
+++ clinicaDental/src/pages/Signup/Signup.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import jwt_decode from 'jwt-decode';
+import { useState } from 'react';
+import { useNavigate, Link } from "react-router-dom";
+import { signup } from '../../services/apiCalls';
+import { checkError } from '../../services/checkError';
+import '../Signup/Signup.css'
+
+export function Signup() {
+    const [dataUser, setDataUser] = useState({
+        name: '',
+        surname: '',
+        dni: '',
+        email: '',
+        password: ''
+    })
+
+    const [credentialsError, setCredentialsError] = useState({
+        emailError: "",
+        passwordError: "",
+    });
+
+    const [errorClassName, setErrorClassName] = useState("errorNone");
+
+    const navigate = useNavigate();
+
+    const inputHandler = (e) => {
+        setDataUser((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const inputCheck = (e) => {
+        let mensajeError = checkError(e.target.name, e.target.value);
+        setCredentialsError((prevState) => ({
+            ...prevState,
+            [e.target.name + "Error"]: mensajeError,
+        }));
+    };
+
+    const singupUser = () => {
+        signup(dataUser)
+            .then((result) => {
+                console.log(result)
+                console.log(result.data.token);
+                let tokenDecoded = jwt_decode(result.data.token);
+                console.log(tokenDecoded);
+
+                setTimeout(() => {
+                    navigate("/");
+                }, 2000);
+
+            })
+            .catch(error => {
+                console.log(error.response.status);
+                if (error.response.status === 400) {
+                    setErrorClassName ('errorShown')
+                }
+                else {
+                    let errorClassName= 'errorClassName'
+                }
+            })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        singupUser();
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit} className='signup-form'>
+                <input
+                    name='name' placeholder='Name' type='text' onChange={(e) => inputHandler(e)} className="login-input" />
+                <input
+                    name='surname' placeholder='Surname' type='text' onChange={(e) => inputHandler(e)} className="login-input" />
+                <input
+                    name='dni' placeholder='Identity number' type='text' onChange={(e) => inputHandler(e)} onBlur={(e) => inputCheck(e)} className="login-input" />
+                <input
+                    name='email' placeholder='email@email.com' type='email' onChange={(e) => inputHandler(e)} onBlur={(e) => inputCheck(e)} className="login-input" />
+                <div className="errorText">{credentialsError.emailError}</div>
+                <input
+                    name='password' placeholder='*******' type='password' onChange={(e) => inputHandler(e)} onBlur={(e) => inputCheck(e)} className="login-input" />
+                <div className="errorText">{credentialsError.passwordError}</div>
+                <div className={errorClassName}>No se puede registrar</div>
+                <button type='submit'>Registro</button>
+                <span>Al continuar, aceptas nuestros</span>
+                <Link to='/' className='link-text'>Términos de Uso y Política de Privacidad</Link>
+            </form>
+        </>
+    )
+}
--- clinicaDental/src/services/apiCalls.js
+++ clinicaDental/src/services/apiCalls.js
@@ -5,3 +5,7 @@ const ENDPOINT = 'http://localhost:3000';
 export const login = async (credentials) => {
     return await axios.post(`${ENDPOINT}/login`, credentials)
 }
+
+export const signup = async (dataUser) => {
+    return await axios.post(`${ENDPOINT}/signup`, dataUser)
+}
