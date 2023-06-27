7e1c950f1fc8f271f893aae63bd38ede5a664c98
DitGrau <juditgraup@gmail.com>
2023-06-27 15:49:54 +0200

    "feat: conexion con la API en myProfile y con Redux"

--- clinicaDental/src/pages/Profile/Profile.jsx
+++ clinicaDental/src/pages/Profile/Profile.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useSelector } from 'react-redux';
 import { IconNav } from '../../componentes/IconNav/IconNav'
 import { Container, Row, Col } from 'react-bootstrap'
+import { userData } from '../../Redux/userSlice';
+import { getMyProfile } from '../../services/apiCalls';
+
 import '../Profile/Profile.css'
+
+
 import userIcon from '../../assets/user-circle.svg'
 import editIcon from '../../assets/writing.svg'
-import { MDBCard, MDBCardBody, MDBCardFooter, } from 'mdb-react-ui-kit';
 
 
 export function Profile() {
+
+
+
+    const dataSlice = useSelector(userData);
+    const isLogged = (!!dataSlice.credentials.token)
+    const token = dataSlice?.credentials?.token;
+    console.log (isLogged)
+    console.log (dataSlice)
+    console.log (token)
+
+useEffect(()=> {
+    getMyProfile(token).then ((res)=>{
+        console.log(res)
+    })
+}
+
+)
+
     return (
         <Container>
             <Row className="profile-row">
                 <Col xs={10} md={4} className="profile-card">
-
                     <aside className="id-card">
                         <img src={userIcon} alt='userProfile'></img>
                         <span>id: 167809</span>
@@ -40,9 +62,6 @@ export function Profile() {
                             <IconNav link='/nuevaCita' className='whiteStyle' icon={editIcon} text='Pedir cita' />
                         </div>
                     </section>
-
-
-
                 </Col>
             </Row>
         </Container >
