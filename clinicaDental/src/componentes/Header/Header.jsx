b160891aa40d7174f4fd6fc96e5683743b501adf
DitGrau <juditgraup@gmail.com>
2023-06-28 19:49:52 +0200

    "fix: nombre navbar, clean up"

--- clinicaDental/src/componentes/Header/Header.jsx
+++ clinicaDental/src/componentes/Header/Header.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Logo } from '../Logo/Logo';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout, userData } from '../../Redux/userSlice';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../Redux/userSlice';
 import { IconNav } from '../IconNav/IconNav'
 import { getMyProfile } from '../../services/apiCalls';
 import loginIcon from '../../assets/door-enter.svg'
 import signinIcon from '../../assets/user-plus.svg'
 import logoutIcon from '../../assets/door-exit.svg'
 import userIcon from '../../assets/user-circle.svg'
-
+import { Token } from "../../services/dataFromSlice";
 import './Header.css'
 
 ///////////////////////////////////////////////
@@ -18,17 +18,22 @@ export function Header() {
 
     const [name, setName] = useState('');
     const dispatch = useDispatch();
-    const dataSlice = useSelector(userData)
+    const token = Token()
 
-    const isLogged = (!!dataSlice.credentials.token)
-    const token = dataSlice?.credentials?.token;
+    const isLogged = (!!token)
+    
 
     useEffect(() => {
-        getMyProfile(token).then((res) => {
-            setName(`${(res.name).toUpperCase()}`)
-        })
-    }, [])
-
+        if (!token) {
+            setName ('');
+        } else {
+            getMyProfile(token).then((res) => {
+                setName(`${(res.name).toUpperCase()}`)
+            })
+        }
+    }, [token])
+ 
+    console.log (name)
     return (
         <Navbar className='navbarMain' style={{ justifyContent: 'space-between' }}>
             <Logo />
