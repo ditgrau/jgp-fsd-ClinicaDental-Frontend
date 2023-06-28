4194a741b90a4a3a79e8fc7cabbb6555c33180fe
DitGrau <juditgraup@gmail.com>
2023-06-28 10:23:55 +0200

    "style: myprofile y detalle nombre header"

--- clinicaDental/src/componentes/Header/Header.jsx
+++ clinicaDental/src/componentes/Header/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Logo } from '../Logo/Logo';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, userData } from '../../Redux/userSlice';
 import { IconNav } from '../IconNav/IconNav'
+import { getMyProfile } from '../../services/apiCalls';
 import loginIcon from '../../assets/door-enter.svg'
 import signinIcon from '../../assets/user-plus.svg'
 import logoutIcon from '../../assets/door-exit.svg'
@@ -15,10 +16,20 @@ import './Header.css'
 
 export function Header() {
 
+    const [user, setUser] = useState({});
     const dispatch = useDispatch();
     const dataSlice = useSelector(userData)
 
     const isLogged = (!!dataSlice.credentials.token)
+    const token = dataSlice?.credentials?.token;
+
+    useEffect(() => {
+        getMyProfile(token).then((res) => {
+            setUser(res)
+        })
+    }, [])
+
+    console.log(user)
 
     return (
         <Navbar className='navbarMain' style={{ justifyContent: 'space-between' }}>
@@ -26,6 +37,7 @@ export function Header() {
             {
                 isLogged
                     ? <nav className='nav-icons'>
+                        <span className='nav-userName'>{user.name} {user.surname}</span>
                         <IconNav link='/profile' className='whiteStyle' icon={userIcon} text='Mi Perfil'/>
                         <IconNav link='/' className='whiteStyle' icon={logoutIcon}  text='Logout' clickFunction={() => dispatch(logout())}/>
                     </nav>
--- clinicaDental/src/pages/Profile/Profile.jsx
+++ clinicaDental/src/pages/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from 'react-redux';
 import { IconNav } from '../../componentes/IconNav/IconNav'
 import { Container, Row, Col } from 'react-bootstrap'
@@ -17,53 +17,69 @@ export function Profile() {
     const [user, setUser] = useState({});
     const [editing, setEditing] = useState(false);
     const [body, setBody] = useState({});
+    const [letra, setLetra] = useState('');
 
     const dataSlice = useSelector(userData);
     const isLogged = (!!dataSlice.credentials.token)
     const token = dataSlice?.credentials?.token;
-    console.log (isLogged)
-    console.log (dataSlice)
-    console.log (token)
+    const isDentist = (!!user.Dentist)
 
-useEffect(()=> {
-    getMyProfile(token).then ((res)=>{
-        setUser(res)
-        
-    }) 
-}, [])
-console.log(user.id)
+    useEffect(() => {
+        getMyProfile(token).then((res) => {
+            setUser(res)
+            setLetra((res.name)[0].toUpperCase());
+            console.log(letra)
+        })
+    }, [])
+    console.log(user.Dentist)
+    console.log(letra)
+
 
     return (
         <Container>
             <Row className="profile-row">
                 <Col xs={10} md={4} className="profile-card">
-                    <aside className="id-card">
-                        <img src={userIcon} alt='userProfile'></img>
-                        <span>{user.id}</span>
-                    </aside>
+
+                    <section className="id-card">
+                        <div className='capital-letter'><div>{letra}</div></div>
+                        <span className='id-number'>id {user.id}</span>
+                        <span className="title-profile"> {user.name} {user.surname}</span>
+                        <span className="subtitle-profile">MI PERFIL</span>
+                    </section>
+
                     <section className="data-card">
                         <div>
-                            <p className="title-card">Nombre</p>
-                            <span>{user.name}</span>
+                            <span className="title-card">Nombre</span><span className="info-user">{user.name}</span>
                         </div>
                         <div>
-                            <p className="title-card">Apellidos</p>
-                            <span>{user.surname}</span>
+                            <span className="title-card">Apellidos</span><span className="info-user">{user.surname}</span>
                         </div>
                         <div>
-                            <p className="title-card">DNI</p>
-                            <span>{user.dni}</span>
+                            <span className="title-card">DNI</span><span className="info-user">{user.dni}</span>
                         </div>
                         <div>
-                            <p className="title-card">email</p>
-                            <span>{user.email}</span>
+                            <span className="title-card">Email</span><span className="info-user">{user.email}</span>
                         </div>
-                        <div className="profile-footer">
-                            <IconNav link='/edit' className='whiteStyle' icon={editIcon} text='Editar' />
-                            <IconNav link='/citas' className='whiteStyle' icon={editIcon} text='Mis citas' />
-                            <IconNav link='/nuevaCita' className='whiteStyle' icon={editIcon} text='Pedir cita' />
-                        </div>
+
+                        {
+                            isDentist
+                                ?
+                                <>
+                                    <div>
+                                        <span className="title-card">Collegiate</span><span className="info-user">{user.Dentist.collegiate}</span>
+                                    </div>
+                                    <div>
+                                        <span className="title-card">Specialty</span><span className="info-user">{user.Dentist.Specialty.name}</span>
+                                    </div>
+                                </>
+                                : <></>
+                        }
                     </section>
+                    <div className="profile-footer">
+                        <IconNav link='/nuevaCita' className='whiteStyle' icon={editIcon} text='Pedir cita' />
+                        <IconNav link='/citas' className='whiteStyle' icon={editIcon} text='Mis citas' />
+                        <IconNav link='/edit' className='whiteStyle' icon={editIcon} text='Editar' />
+                    </div>
                 </Col>
             </Row>
         </Container >
